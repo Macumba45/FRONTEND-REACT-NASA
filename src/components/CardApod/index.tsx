51a4898b07a88ad62c1bcce2e95fb4623f2ecce9
Macumba45 <gonzalolovo@gmail.com>
2023-02-17 18:55:39 +0100

    Delete elements and render once deleted

--- src/components/CardApod/index.tsx
+++ src/components/CardApod/index.tsx
@@ -1,45 +1,27 @@
-import { FC, useContext, useEffect, useState } from "react";
-import Explanation from "./Explanation";
+import { FC, useCallback } from "react";
 import Title from "./Title";
 import Date from "./Date";
 import { Props } from "./type";
 import Url from "./Url";
 import { Content, ContentButtons, DeteleApod } from "./styles";
 import { getAuthenticatedToken } from "../../services/storage";
-import Apod from "../../views/Apod";
+
 
 const CardApod: FC<Props> = ({ id, title, date, url, onRemove }) => {
 
 
-    const printApods = async () => {
-
-        const token = getAuthenticatedToken(); // Obtener el token de localStorage
-        await fetch('http://localhost:8000/apods/', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}` // Agregar el token al header 'Authorization'
-            },
-        })
-
-    }
-
-    const deleteApod = async (id: number) => {
-
-
+    const deleteApod = useCallback(async (id: number) => {
         const token = getAuthenticatedToken();             // Obtener el token de localStorage
         await fetch(`http://localhost:8000/apods/${id}`, {
-
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}` // Agregar el token al header 'Authorization'
             },
         });
-
         onRemove(id);
+    }, [onRemove]);
 
-    }
 
 
     return (
@@ -52,7 +34,7 @@ const CardApod: FC<Props> = ({ id, title, date, url, onRemove }) => {
             <Date date={date}
             />
             <ContentButtons>
-                <DeteleApod type="button" onClick={() => { deleteApod(id); printApods() }}>Delete</DeteleApod>
+                <DeteleApod type="button" onClick={() => { deleteApod(id) }}>Delete</DeteleApod>
                 <DeteleApod type="button" onClick={() => { }}>Add to Fav</DeteleApod>
             </ContentButtons>
         </Content>
